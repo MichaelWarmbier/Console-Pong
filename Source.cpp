6efbef0583e1505af8836d64b16f41b3894be4ad
Michael Warmbier <MichaelCWarmbier@gmail.com>
2020-11-21 15:57:54 -0500

    Fixed mistakes

--- Source.cpp
+++ Source.cpp
@@ -7,27 +7,25 @@ using namespace std;
 COORD screen;											// Screen Coordinates (Windows Only)
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);		// Handle For Console Window
 
-const int Dimensions[2] = { 15, 40 };							// Board Height & Width (h, w)
-int Ball_P[2] = { Dimensions[1] / 2, Dimensions[0] / 2 };		// Ball Position (x,y)
+const int HEIGHT = 10, WIDTH = 60;								// Board Height & Width (h, w)
+int Ball_P[2] = { WIDTH / 2, HEIGHT / 2 };						// Ball Position (x,y)
 int Ball_V[2] = { 0,0 };										// Ball Direction (x,y)
-int Paddle_P[2] = { Dimensions[0] / 2,  Dimensions[0] / 2 };	// Paddle Positions (l, r)
+int Paddle_P[2] = { HEIGHT / 2,  HEIGHT / 2 };					// Paddle Positions (l, r)
 int Score[2] = { 0,0 };											// Scores (l, r)
-int AI = 3;														// AI Determiner (higher => easier)
+int AI = 2;														// AI Determiner (higher => easier)
 
 
 void Draw(), Input(), Logic();		// Main Routines
 void ResetBall();					// Secondary Routine
 
 int main() {		
-	screen.Y = 0;
-	screen.X = 0;
+	srand(time(0));		// Random Seed
+	screen.Y = 0; screen.X = 0;
 
 	ResetBall();								
 
 	while (true) {
-		Draw();
-		Input();
-		Logic();
+		Draw(); Input(); Logic();					// Logic Loop (Frame)
 		SetConsoleCursorPosition(console, screen);	// Reset Screen Drawing
 		Sleep(100);									// Buffer Frames
 	}
@@ -36,12 +34,12 @@ int main() {
 
 void Draw() {		// Draw characters to screen
 	
-	for (int i = 0; i < Dimensions[1]; i++) {		// Score
+	for (int i = 0; i < WIDTH; i++) {		// Score
 
 		if (i == 8)
 			cout << Score[0];
 
-		else if (i == Dimensions[1] - 9)
+		else if (i == WIDTH - 9)
 			cout << Score[1];
 
 		else
@@ -50,27 +48,27 @@ void Draw() {		// Draw characters to screen
 
 	cout << endl;
 
-	for (int y = 0; y < Dimensions[0]; y++) {
+	for (int y = 0; y < HEIGHT; y++) {
 
-		for (int x = 0; x < Dimensions[1]; x++) {
+		for (int x = 0; x < WIDTH; x++) {
 
-			if (y == 0 || y == Dimensions[0] - 1)															// Top & Bottom Border
-				cout << '#';
+			if (y == 0 || y == HEIGHT - 1)																	// Top & Bottom Border
+				cout << '=';
 
-			else if (x == 0 && (y == Paddle_P[0] || y == Paddle_P[0] + 1 || y == Paddle_P[0] - 1))				// Left Paddle
+			else if (x == 0 && (y == Paddle_P[0] || y == Paddle_P[0] + 1 || y == Paddle_P[0] - 1))					// Left Paddle
 				cout << ']';
 
-			else if (x == Dimensions[1] - 1 && (y == Paddle_P[1] || y == Paddle_P[1] + 1 || y == Paddle_P[1] - 1))		// Right Paddle
+			else if (x == WIDTH - 1 && (y == Paddle_P[1] || y == Paddle_P[1] + 1 || y == Paddle_P[1] - 1))	// Right Paddle
 				cout << '[';
 
-			else if (y == Ball_P[1] && x == Ball_P[0])													// Ball
+			else if (y == Ball_P[1] && x == Ball_P[0])																// Ball
 				cout << 'O';
 
-			else if (x == (Dimensions[1] / 2))																// Center Line
+			else if (x == (WIDTH / 2))																		// Center Line
 				cout << "|";
 
 			else
-				cout << ' ';																			// Empty Space
+				cout << ' ';																						// Empty Space
 		}
 		cout << endl;
 	}
@@ -92,12 +90,10 @@ void Input() {		// Logic For Input
 		}
 
 	/* AI Controlled Paddle */
-	srand(time(0));		// Random Seed
-
 	if (Paddle_P[1] < Ball_P[1] && rand() % AI == 0)
 		Paddle_P[1]++;
 
-	if (Paddle_P[1] > Ball_P[1])
+	if (Paddle_P[1] > Ball_P[1] && rand() % AI == 0)
 		Paddle_P[1]--;
 
 }
@@ -107,12 +103,12 @@ void Logic() {		// Regular Logic
 	/* Paddle Limits */
 	if (Paddle_P[0] < 2)
 		Paddle_P[0] = 2;
-	if (Paddle_P[0] > Dimensions[0] - 3)
-		Paddle_P[0] = Dimensions[0] - 3;
+	if (Paddle_P[0] > HEIGHT - 3)
+		Paddle_P[0] = HEIGHT - 3;
 	if (Paddle_P[1] < 2)
 		Paddle_P[1] = 2;
-	if (Paddle_P[1] > Dimensions[0] - 3)
-		Paddle_P[1] = Dimensions[0] - 3;
+	if (Paddle_P[1] > HEIGHT - 3)
+		Paddle_P[1] = HEIGHT - 3;
 
 	/* Velocity */
 	Ball_P[0] += Ball_V[0];
@@ -122,14 +118,14 @@ void Logic() {		// Regular Logic
 	if (Ball_P[0] == 0 && (Ball_P[1] == Paddle_P[0] || Ball_P[1] == Paddle_P[0] + 1 || Ball_P[1] == Paddle_P[0] - 1))
 		Ball_V[0] *= -1;
 
-	if (Ball_P[0] == Dimensions[1] - 1 && (Ball_P[1] == Paddle_P[1] || Ball_P[1] == Paddle_P[1] + 1 || Ball_P[1] == Paddle_P[1] - 1))
+	if (Ball_P[0] == WIDTH - 1 && (Ball_P[1] == Paddle_P[1] || Ball_P[1] == Paddle_P[1] + 1 || Ball_P[1] == Paddle_P[1] - 1))
 		Ball_V[0] *= -1;
 
-	if (Ball_P[1] == 0 || Ball_P[1] == Dimensions[0] - 1)
+	if (Ball_P[1] == 0 || Ball_P[1] == HEIGHT - 1)
 		Ball_V[1] *= -1;
 		
 	/* Scoring */
-	if (Ball_P[0] > Dimensions[1] - 1) {
+	if (Ball_P[0] > WIDTH - 1) {
 		Score[0]++;
 		ResetBall();
 	}
@@ -144,14 +140,13 @@ void Logic() {		// Regular Logic
 
 void ResetBall() {		// Resets Ball To Default Position & Determines Velocity Direction
 
-	srand(time(0));		// Random Seed
 
 	/* Reset Ball Positions To Center Board */
-	Ball_P[0] = Dimensions[1] / 2;		
-	Ball_P[1] = Dimensions[0] / 2;
+	Ball_P[0] = WIDTH / 2;		
+	Ball_P[1] = HEIGHT / 2;
 
 	/* Determine Balls Y Direction (Random) */
-	if (rand() % 2 == 1)
+	if (rand() % 2 == 1) 
 		Ball_V[1] = -1;
 	else
 		Ball_V[1] = 1;
